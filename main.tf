49d2fecc87227b5ce0d7f2b85adf603592bc061e
Nikhil Anil Patil <nikhilanilpatil@gmail.com>
2021-10-03 21:19:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
 
   tags = {

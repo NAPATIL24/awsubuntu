90c5ba18b6f7addf09d4214390ed3f6a7aa8acf7
Nikhil Anil Patil <nikhilanilpatil@gmail.com>
2021-10-03 20:55:55 +0530

    Update and rename awsubuntu.tf to main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
-  access_key = var.accesskey
-  secret_key = var.secretkey
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "app_server" {
